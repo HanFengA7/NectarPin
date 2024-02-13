b7f1b9cdc81d53e569060a03ef6eb2d4888c5396
HanFengA7 <tancan137@foxmail.com>
2024-02-13 23:53:51 +0800

    feat: 快捷入口跳转

--- web/NectarPin-Admin/src/components/Article/add.vue
+++ web/NectarPin-Admin/src/components/Article/add.vue
@@ -3,14 +3,23 @@ import eventBus from "@/plugin/event-bus/event-bus";
 import {ref} from "vue";
 import router from "@/router";
 
+
+/*
+接收父组件数据
+*/
+//[1]UserInfoData数据
+const props = defineProps(['userInfo']);
+
 /*
 传数据给父组件
-[1]设置侧边栏选择选项
 */
+//[1]设置侧边栏选择选项
 let SelectedKeys: any = ref(["Article/add"]);
 eventBus.emit("child-data-selectedKeys", SelectedKeys);
 
-//[HeardCardOnBack] 返回函数
+/*
+[HeardCardOnBack] 返回函数
+*/
 const HeardCardOnBack = () => {
   router.push({name: 'Dashboard'})
 }
@@ -38,6 +47,9 @@ const HeardCardOnBack = () => {
       </template>
     </a-page-header>
   </div>
+
+  <br/>
+
 </template>
 
 <style scoped>
