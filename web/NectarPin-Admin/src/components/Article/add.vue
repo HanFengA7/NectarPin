2ef1836b1a5ccc69c26dc8e92818b181d69b7a1e
HanFengA7 <tancan137@foxmail.com>
2024-02-15 16:54:55 +0800

    feat:文章编辑

--- web/NectarPin-Admin/src/components/Article/add.vue
+++ web/NectarPin-Admin/src/components/Article/add.vue
@@ -1,6 +1,6 @@
 <script setup>
 import eventBus from "@/plugin/event-bus/event-bus";
-import {reactive, ref} from "vue";
+import {reactive, ref, watch, watchEffect} from "vue";
 import router from "@/router";
 
 /*
@@ -35,31 +35,30 @@ const TagsHandleChange = () => {
 //文章评论开关
 const aif_commentRef = ref(true);
 const aif_commentHandleChange = () => {
-  if (aif_commentRef.value === true){
+  if (aif_commentRef.value === true) {
     ArticleForm.aif_comment = 1
-  }else {
+  } else {
     ArticleForm.aif_comment = 0
   }
 };
 //文章隐藏开关
 const aif_hideRef = ref(false);
 const aif_hideHandleChange = () => {
-  if (aif_hideRef.value === true){
+  if (aif_hideRef.value === true) {
     ArticleForm.aif_hide = 1
-  }else {
+  } else {
     ArticleForm.aif_hide = 0
   }
 };
-//文章加密开关|密码
+//文章加密开关
 const aif_encryptRef = ref(false);
 const aif_encryptHandleChange = () => {
-  if (aif_encryptRef.value === true){
+  if (aif_encryptRef.value === true) {
     ArticleForm.aif_encrypt = 1
-  }else {
+  } else {
     ArticleForm.aif_encrypt = 0
   }
 };
-
 //文章数据
 const ArticleForm = reactive({
   uid: props.userInfo["id"],
@@ -74,6 +73,61 @@ const ArticleForm = reactive({
   aif_encrypt: 0,
   aif_encrypt_pwd: '',
 })
+
+//文章数据重置
+const RestArticleForm = () => {
+  ArticleForm.uid = props.userInfo["id"]
+  ArticleForm.cid = '1'
+  ArticleForm.title = ''
+  ArticleForm.desc = ''
+  ArticleForm.content = ''
+  ArticleForm.img_url = ''
+  ArticleForm.aif_comment = 1
+  ArticleForm.aif_hide = 0
+  ArticleForm.aif_encrypt = 0
+  ArticleForm.aif_encrypt_pwd = ''
+}
+
+//
+// let IFIMGExistsKey = ref(new Date())
+// const IFIMGExists = () => {
+//   let image = new Image();
+//
+//   // watch(ArticleForm.img_url, (newValue, oldValue) => {
+//   //   image.onerror = function () {
+//   //     imageV = false
+//   //   };
+//   //   image.onload = function () {
+//   //     imageV = true
+//   //   };
+//   //   image.src = newValue
+//   //   return imageV
+//   // })
+//   return imageV
+// }
+
+//判断图片是否存在
+let imageV = ref(0)
+watch(() => ArticleForm.img_url, (newValue) => {
+  if (newValue === "") {
+    imageV.value = 0
+  } else {
+    let image = new Image();
+    image.onerror = function () {
+      imageV.value = 2
+    }
+    image.onload = function () {
+      imageV.value = 1
+    }
+    image.src = newValue
+  }
+});
+
+//发布文章
+const PostArticle = (data) => {
+  console.log(data)
+}
+
 
 /*
 传数据给父组件
@@ -91,34 +145,39 @@ const HeardCardOnBack = () => {
 </script>
 
 <template>
-  <div class="ArticleAdd-heard-PC">
-    <a-page-header
-        title="撰写文章"
-        subtitle="WriteArticle"
-        :show-back="true"
-        @back=HeardCardOnBack
-    >
-      <template #breadcrumb>
-        <a-breadcrumb>
-          <a-breadcrumb-item>后台</a-breadcrumb-item>
-          <a-breadcrumb-item>撰写文章</a-breadcrumb-item>
-        </a-breadcrumb>
-      </template>
+  <a-form
+      :model="ArticleForm"
+      @submit="PostArticle"
+  >
 
-      <template #extra>
-        <a-space>
-          <a-button @click="">重置文章</a-button>
-        </a-space>
-      </template>
-    </a-page-header>
-  </div>
+    <div class="ArticleAdd-heard-PC">
+      <a-page-header
+          title="撰写文章"
+          subtitle="WriteArticle"
+          :show-back="true"
+          @back=HeardCardOnBack
+      >
+        <template #breadcrumb>
+          <a-breadcrumb>
+            <a-breadcrumb-item>后台</a-breadcrumb-item>
+            <a-breadcrumb-item>撰写文章</a-breadcrumb-item>
+          </a-breadcrumb>
+        </template>
 
-  <div class="ArticleAdd-body-PC">
-    <a-form :model="ArticleForm">
+        <template #extra>
+          <a-space>
+            <a-button html-type="submit">发布文章</a-button>
+            <a-button @click="RestArticleForm">重置文章</a-button>
+          </a-space>
+        </template>
+      </a-page-header>
+    </div>
+
+    <div class="ArticleAdd-body-PC">
       <div class="ArticleAdd-body-config-PC">
         <a-row>
           <a-col :span="12">
-            <a-form-item field="title" label="文章标题" label-col-flex="100px">
+            <a-form-item field="title" label="文章标题" label-col-flex="80px">
               <a-input v-model="ArticleForm.title"/>
             </a-form-item>
           </a-col>
@@ -130,7 +189,7 @@ const HeardCardOnBack = () => {
             </a-form-item>
           </a-col>
           <a-col :span="12">
-            <a-form-item field="desc" label="文章简介" label-col-flex="100px">
+            <a-form-item field="desc" label="文章简介" label-col-flex="80px">
               <a-textarea v-model="ArticleForm.desc" :max-length="200" :show-word-limit="true" auto-size/>
             </a-form-item>
           </a-col>
@@ -141,7 +200,7 @@ const HeardCardOnBack = () => {
             </a-form-item>
           </a-col>
           <a-col :span="12">
-            <a-form-item field="img_url" label="文章展图" label-col-flex="100px">
+            <a-form-item field="img_url" label="文章展图" label-col-flex="80px">
               <a-input v-model="ArticleForm.img_url" placeholder="填写一个图片外链"/>
             </a-form-item>
           </a-col>
@@ -151,7 +210,7 @@ const HeardCardOnBack = () => {
                   type="round"
                   v-model="aif_commentRef"
                   @change="aif_commentHandleChange"
-                  />
+              />
             </a-form-item>
           </a-col>
           <a-col :span="4">
@@ -173,19 +232,36 @@ const HeardCardOnBack = () => {
             </a-form-item>
           </a-col>
           <a-col :span="12" v-if="ArticleForm.aif_encrypt === 1">
-            <a-form-item field="aif_encrypt_pwd" label="文章密码" label-col-flex="100px">
+            <a-form-item field="aif_encrypt_pwd" label="文章密码" label-col-flex="80px">
               <a-input-password v-model="ArticleForm.aif_encrypt_pwd"/>
             </a-form-item>
+          </a-col>
+
+          <a-col v-show="imageV === 1" :span="24" class="ArticleAdd-WZIMG-config-1-PC">
+            <div class="ArticleAdd-WZIMG-config-2-PC">
+              <a-image
+                  :height="250"
+                  :src=ArticleForm.img_url
+              />
+            </div>
+          </a-col>
+          <a-col v-show="imageV === 2" :span="24" label-col-flex="100px">
+            <a-empty>
+              <template #image>
+                <icon-exclamation-circle-fill/>
+              </template>
+              文章展图资源异常！
+            </a-empty>
           </a-col>
         </a-row>
       </div>
       <v-md-editor
           left-toolbar="undo redo clear | h bold italic strikethrough quote | ul ol table hr | link image code emoji todo-list | save"
-          v-model="ArticleForm.Content"
+          v-model="ArticleForm.content"
           height="700px"
       />
-    </a-form>
-  </div>
+    </div>
+  </a-form>
 
 
 </template>
@@ -206,7 +282,19 @@ const HeardCardOnBack = () => {
 .ArticleAdd-body-config-PC {
   padding: 20px;
 }
-.v-md-editor{
-  border-radius: 18px;
+.ArticleAdd-WZIMG-config-1-PC{
+  padding: 15px 100px;
+}
+.ArticleAdd-WZIMG-config-2-PC{
+  box-sizing: border-box;
+  width: 100%;
+  padding: 15px;
+  text-align: center;
+  border-radius: 5px;
+  background: #f2f3f5;
+}
+
+.v-md-editor {
+  border-radius: 18px 18px 0;
 }
 </style>
